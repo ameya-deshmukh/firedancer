a6b86606c04597e06e45fbf219723e760a094fac
Richard Patel <ripatel@jumptrading.com>
2023-03-03 13:20:38 +0000

    build, ci: add native CC config
    Fixes GitHub CI incompatibility problems by providing an -march=native
    profile.

--- src/ballet/bmtree/Local.mk
+++ src/ballet/bmtree/Local.mk
@@ -4,4 +4,6 @@ $(call add-objs,fd_bmtree,fd_ballet)
 ifdef FD_HAS_ALLOCA
 $(call make-unit-test,test_bmtree20,test_bmtree20,fd_ballet fd_util)
 $(call make-unit-test,test_bmtree32,test_bmtree32,fd_ballet fd_util)
+$(call run-unit-test,test_bmtree20)
+$(call run-unit-test,test_bmtree32)
 endif
